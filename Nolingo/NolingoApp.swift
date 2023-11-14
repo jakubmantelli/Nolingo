5e66fd3fc082a217d707c39b87ac2c5c765b405a
Jakub Mantelli <jakubmantelli@gmail.com>
2023-11-14 10:51:47 +0100

    Initial Commit

---
+++ Nolingo/NolingoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NolingoApp.swift
+//  Nolingo
+//
+//  Created by Jakub Mantelli on 14/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NolingoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
